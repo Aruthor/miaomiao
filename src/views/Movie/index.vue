550f4dfe82e0cce03bc0057509caadb681091fd7
shekaiz <826292460@qq.com>
2019-06-27 00:24:39 +0800

    部分组件以及子路由配置

--- src/views/Movie/index.vue
+++ src/views/Movie/index.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="main">
     <movieHead title="奇趣电影"/>
-    <nowPlaying/>
+      <div id="content">
+        <div class="movie_menu">
+          <router-link tag="div" to="/movie/city" class="city_name">
+            <span>大连</span><i class="iconfont icon-lower-triangle"></i>
+          </router-link>
+          <div class="hot_swtich">
+            <router-link tag="div" to="/movie/nowPlaying" class="hot_item">正在热映</router-link>
+            <router-link tag="div" to="/movie/comingSoon" class="hot_item">即将上映</router-link>
+          </div>
+          <router-link tag="div" to="/movie/search" class="search_entry">
+            <i class="iconfont icon-sousuo"></i>
+          </router-link>
+        </div>
+      </div>
+      <!--子级渲染(单点渲染)-->
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     <movieTabBar/>
   </div>
 </template>
@@ -9,14 +26,12 @@
 <script>
 import movieHead from '@/components/Header';
 import movieTabBar from '@/components/TabBar';
-import nowPlaying from '@/components/NowPlaying';
 
 export default {
   name: 'Movie',
   components:{
     movieHead,
     movieTabBar,
-    nowPlaying
   },
   data() { 
     return {
@@ -27,5 +42,16 @@ export default {
 </script>
 
 <style lang="" scoped>
- 
+#content .movie_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-between; align-items:center; background:white; z-index:10;}
+.movie_menu .city_name{ margin-left: 20px; height:100%; line-height: 45px;}
+.movie_menu .city_name.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .city_name.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .hot_swtich{ display: flex; height:100%; line-height: 45px;}
+.movie_menu .hot_item{ font-size: 15px; color:#666; width:80px; text-align:center; margin:0 12px; font-weight:700;}
+.movie_menu .hot_item.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .hot_item.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .search_entry{ margin-right:20px; height:100%; line-height: 45px;}
+.movie_menu .search_entry.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .search_entry.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
+.movie_menu .search_entry i{  font-size:24px; color:red;}
 </style>
